e640dcaea97f1fb2bc79f1b8c6abc475f9dd3f33
raulglezrdguez <raulglezrguez@gmail.com>
2022-02-01 15:15:36 -0500

    market model created

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const marketsRoutes = require('./routes/markets');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+const httpServer = http.createServer(app);
+
+app.use('/api', gatewaysRoutes);
+app.use('/api', peripheralsRoutes);
+
+mongoose
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('mongodb connected');
+    return httpServer.listen({ port: process.env.PORT || 4000 });
+  })
+  .then(() => {
+    console.log(`Server ready at http://localhost:${process.env.PORT || 4000}`);
+  });
---
+++ models/market.js
@@ -0,0 +1,25 @@
+const { model, Schema } = require('mongoose');
+
+const marketSchema = new Schema({
+  id: {
+    type: Schema.Types.ObjectId,
+    default: function () {
+      return this._id;
+    },
+  },
+  symbol: { type: String, required: true },
+  name: { type: String, required: true },
+  country: { type: String, required: true },
+  industry: { type: String, required: true },
+  ipoYear: { type: Number, required: true },
+  marketCap: { type: Number, required: true },
+  sector: { type: String, required: true },
+  volume: { type: Number, required: true },
+  netChange: { type: Number, required: true },
+  netChangePercent: { type: Number, required: true },
+  lastPrice: { type: Number, required: true },
+  createdAt: { type: String, required: true },
+  updatedAt: { type: String, required: true },
+});
+
+module.exports = model('Market', marketSchema);
